fcc2a053c2bea83b19c0e5059f3cdbfbffd49a14
PidgeyL <pieterjan.moreels@gmail.com>
2015-06-18 23:59:37 +0200

    add treeview like checkbox structure

---
+++ web/static/js/custom/grabable.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('[id^="chk:"]').change(function(){
+    id=$(this).attr("id");
+    if($(this).is(":checked")){
+      $('[id^="'+id+'"]').prop("checked", true);
+    }else{
+      split=id.split("|")
+      if(split.length>1){$('[id="'+split[0]+'"]').prop("checked",false);}
+      if(split.length>2){$('[id="'+split[0]+"|"+split[1]+'"]').prop("checked",false);}
+    }
+  })
+})
